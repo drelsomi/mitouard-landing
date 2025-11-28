8fd79322fec0d7bfa321f3065ef2dc4f374be78e
Chandrel SOMI <chandrelsomi@chandrels-mbp.home>
2025-11-28 16:27:58 +0100

    fix config

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,17 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/styles")],
   },
+  async headers() {
+    return [
+      {
+        source: "/assets/app-release.apk",
+        headers: [
+          { key: "Content-Type", value: "application/vnd.android.package-archive" },
+          { key: "Content-Disposition", value: "attachment; filename=\"app-release.apk\"" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
